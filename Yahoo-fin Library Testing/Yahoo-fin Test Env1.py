e9fd00d8c3a1ce5fea922f265119860088d1114c
Mantton <50926838+Mantton@users.noreply.github.com>
2019-06-08 16:12:48 +0100

    Update Yahoo-fin Test Env1.py

--- Yahoo-fin Library Testing/Yahoo-fin Test Env1.py
+++ Yahoo-fin Library Testing/Yahoo-fin Test Env1.py
@@ -3,8 +3,11 @@
 from yahoo_fin.stock_info import *
 
 
-value = get_live_price('AAPL')
+value = get_live_price('FLWS')
 
 price = round(value,2)
 
+data = get_data('AAPL', start_date='06/01/2019')
+
 print(price)
+print(data)
