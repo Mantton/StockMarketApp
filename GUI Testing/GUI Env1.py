5577b1bc382fcfb23c759d594ad25944acf831e4
Mantton <50926838+Mantton@users.noreply.github.com>
2019-06-08 15:08:15 +0100

    Add files via upload

---
+++ GUI Testing/GUI Env1.py
@@ -0,0 +1,51 @@
+import tkinter as tk
+import random
+
+window = tk.Tk()
+
+
+window.geometry('400x500')
+window.title('Bonjour')
+
+#  --- Functions----------
+def phrase_gen() :
+    phrases = ['Hello ', 'Bonjour ', ' Sup ', "What's good "]
+    name = str(entry1.get())
+
+    return phrases[random.randint(0, 3)] + name
+def phrase_display():
+    greeting = phrase_gen()
+    # ---text field ------
+    text1 = tk.Text(master =window, height = 10, width = 30 )
+    text1.grid(column= 0, row = 3)
+    text1.insert(tk.END, greeting)
+
+# Label
+
+label1 = tk.Label(text = "Test Env")
+label1.grid(column = 0, row = 0)
+
+
+label2 = tk.Label(text = "What is your name? ")
+label2.grid(column = 0, row = 1)
+
+
+# ----- ENTRIES ------------
+
+entry1 = tk.Entry()
+entry1.grid(column=1, row=1 )
+
+
+# ------- BUTTON-------
+
+button1 = tk.Button(text='Submit', command=phrase_display)
+button1.grid(column=0, row=2)
+
+
+
+
+
+
+
+window.mainloop()
+
